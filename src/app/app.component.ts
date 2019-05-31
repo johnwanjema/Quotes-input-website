44526f7d38d8bf6a923acedc4df2faaf52eae2c5
johnwanjema <jonwanjema@gmail.com>
2019-05-31 10:57:33 +0300

    added a constructor for qoutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Quotes, } from './quotes'
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
-}
+  title = 'Quotes';
+
+  quotes = [
+    new Quotes(1, 'Watch Finding Nemo', `wanje`, 0),
+    new Quotes(2, 'Buy Cookies', `wanje`, 0),
+    new Quotes(3, 'Get new Phone Case', `wanje`, 0),
+    new Quotes(4, 'Get Dog Food', `wanje`, 0),
+    new Quotes(5, 'Solve math homework', `wanje`, 0),
+    new Quotes(6, 'Plot my world domination plan', `wanje`, 0),
+
+  ]
+
+
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,4 @@
 export class Quotes {
+  constructor(public id: number, public name: string, public author: string, public vote: number) { }
 }
+
