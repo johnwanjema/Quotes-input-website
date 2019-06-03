6f6185aff66dbe74370e987c24999631ba003598
johnwanjema <jonwanjema@gmail.com>
2019-06-03 17:47:13 +0300

    Auto-generated commit

--- src/app/quotes.ts
+++
@@ -1,5 +0,0 @@
-export class Quotes {
-  constructor(public id: number, public name: string, public author: string,
-    public upvote: number,  public downvote: number ,public person:String,public date:Date,) { }
-}
-
--- src/app/time-past.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimePastPipe } from './time-past.pipe';
-
-describe('TimePastPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimePastPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
